81df969b9c1dda758b68a4d976fbfa0bc18f9f6b
dial-workable <dial@workable.com>
2019-12-05 14:26:48 +0200

    ADD tutorial part2-2

--- part2/main.go
+++ part2/main.go
@@ -19,4 +19,25 @@ func main() {
 	c1.X = 5
 	fmt.Println(c1) // {5 2}
 	fmt.Println(c2) // {5 2}
+
+	add := func(a, b Coord) Coord {
+		return Coord{a.X + b.X, a.Y + b.Y}
+	}
+	increase := func(a *Coord, b Coord) { // <<< Use pointer as a parameter
+		a.X += b.X
+		a.Y += b.Y
+	}
+
+	c3 := Coord{10, 20}
+	res1 := add(c1, c3)
+	c1.X++
+	fmt.Println(c1)
+	fmt.Println(res1)
+
+	increase(&c1, c3) // <<< Pass a pointer parameter
+	fmt.Println(c1)
+
+	pc3 := &c3
+	increase(&c1, *pc3) // <<< Pass a pointer as a value parameter (pc3)
+	fmt.Println(c1)
 }
