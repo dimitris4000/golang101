1dc431ad3e6f459e63bc4d947c700256854175df
dial-workable <dial@workable.com>
2019-12-05 14:42:32 +0200

    ADD tutorial part2-4 (complete)

--- part2/main.go
+++ part2/main.go
@@ -4,14 +4,26 @@ import (
 	"fmt"
 )
 
-func main() {
-	type Coord struct {
-		X float32
-		Y float32
-	}
+type Coord struct {
+	X float32
+	Y float32
+}
 
-	c1 := Coord{X: 1, Y: 2}
-	c2 := &c1              // <<< Get the memory address to c1 and put it in pc1
+func NewCoord(x, y float32) Coord {
+	return Coord{x, y}
+}
+
+func (c Coord) add(b Coord) Coord { // <<< Use value as a receiver
+	return Coord{c.X + b.X, c.Y + b.Y}
+}
+func (c *Coord) increase(b Coord) { // <<< Use pointer as a receiver
+	c.X += b.X
+	c.Y += b.Y
+}
+
+func main() {
+	c1 := NewCoord(1, 2)
+	c2 := &c1
 	fmt.Printf("%T\n", c2) // *main.Coord
 
 	fmt.Println(c1) // {1 2}
@@ -20,24 +32,16 @@ func main() {
 	fmt.Println(c1) // {5 2}
 	fmt.Println(c2) // {5 2}
 
-	add := func(a, b Coord) Coord {
-		return Coord{a.X + b.X, a.Y + b.Y}
-	}
-	increase := func(a *Coord, b Coord) { // <<< Use pointer as a parameter
-		a.X += b.X
-		a.Y += b.Y
-	}
-
-	c3 := Coord{10, 20}
-	res1 := add(c1, c3)
+	c3 := NewCoord(10, 20)
+	res1 := c1.add(c3)
 	c1.X++
 	fmt.Println(c1)
 	fmt.Println(res1)
 
-	increase(&c1, c3) // <<< Pass a pointer parameter
+	c1.increase(c3)
 	fmt.Println(c1)
 
 	pc3 := &c3
-	increase(&c1, *pc3) // <<< Pass a pointer as a value parameter (pc3)
+	c1.increase(*pc3)
 	fmt.Println(c1)
 }
